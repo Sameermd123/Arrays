2253833810a5602594a9347d290ca6057f092ca4
Md Sameer <146635580+Sameermd123@users.noreply.github.com>
2024-04-09 21:01:35 +0530

    Add files via upload

---
+++ linearSearchIn2DArray.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+bool isPresent(int arr[][4], int target, int n, int m)
+{
+    for(int row=0;row<3;row++)
+    {
+        for(int col=0;col<4;col++)
+        {
+            if(arr[row][col] == target)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+int main()
+{
+    int arr[3][4];
+    for(int row=0;row<3;row++)
+    {
+        for(int col=0;col<4;col++)
+        {
+            cin >> arr[row][col];
+        }
+    }
+    for(int row=0;row<3;row++)
+    {
+        for(int col=0;col<4;col++)
+        {
+            cout << arr[row][col] << " ";
+        }
+        cout<<endl;
+    }
+    int target;
+    cout<<"Enter the element to search for : " <<endl;
+    cin >> target;
+
+    if(isPresent(arr, target, 3, 4))
+    {
+        cout<<"Element found : "<<endl;
+    }
+    else
+    {
+        cout<< "Element not found : "<<endl;
+    }
+    return 0;
+}
